5ccb56eb01f3387b9d8deb954747784d62f9fe7c
DESKTOP-DKO3O52\hklei <hklee6417@gmail.com>
2020-12-02 06:38:21 +0900

    Create table, Mapping

--- pythonBasic/SQLAlchemy/Example1.py
+++ pythonBasic/SQLAlchemy/Example1.py
@@ -1,67 +1,55 @@
-import os
-import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, MetaData
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+
+# 1. 오라클 접속정보
+DIALECT = 'oracle'
+SQL_DRIVER = 'cx_oracle'
+USERNAME = 'pythonTest' #enter your username
+PASSWORD = '1234' #enter your password
+HOST = 'localhost' #enter the oracle db host url
+PORT = 1521 # enter the oracle port number
+SERVICE = 'pythonTest' # enter the oracle db service name
+ENGINE_PATH_WIN_AUTH = DIALECT + '+' + SQL_DRIVER + '://' + USERNAME + ':' + PASSWORD +'@' + HOST + ':' + str(PORT) + '/?service_name=' + SERVICE
+
+# 2. 두부 연결을 위한 engine 생성 - lazy connecting
 from sqlalchemy import create_engine
-import cx_Oracle
+engine = create_engine(ENGINE_PATH_WIN_AUTH)
 
-# 오라클 접속정보
-host = 'pythonTest'
-port = '1521'
-sid = 'xe'
-user = 'pythonTest'
-password = '1234'
-sid = cx_Oracle.makedsn(host, port, sid=sid)
 
-cstr = 'oracle://{user}:{password}@{sid}'.format(
-    user = user
-    , password = password
-    , sid = sid
-)
-# 두부 연결을 위한 engine 생성
-engine = create_engine(
-    cstr
-    , convert_unicode=False
-    , pool_recycle = 10
-    , pool_size = 50
-    , echo = True
-)
+# 3. Mapping Class
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Table, MetaData
 
 Base = declarative_base()
 
-class Person(Base):
-    # 테이블명을 지정한다
-    __tablename__ = 'person'
 
-    # 컬럼정의, 기본키 지정
-    id = Column(Integer, primary_keyu= True)
+class User(Base):
+    __tablename__ = 'users'
 
-    # 일반적인 컬럼 정의
-    name = Column(String(250), nullable=False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    fullname = Column(String(50))
+    nickname = Column(String(50))
 
-metadata = MetaData()
-address = Table('address', metadata,
-    Column('street_name', String(250)),
-    Column('street_member' String(250)),
-    Column('post_code', String(250), nullable = False))
-# class Address(Base):
-#     # 테이블명을 지정한다
-#     __tablename__ = 'address'
+    def __repr__(self):
+        return "<User(name='%s', fullname='%s', nickname='%s')>" \
+               % (self.name, self.fullname, self.nickname)
+
+# print(User.__table__) 메타데이터 확인
+# print(User.__mapper__) 매퍼 클래스 확인
+
+Base.metadata.create_all(engine)
+
+# # 인스턴스 생성
+# ed_user = Userinfo(name='ed', fullname='Ed Jones', nickname='ednickname')
 #
-#     # 컬럼을 정의, 기본키 지정
-#     id = Column(Integer, primary_key=True)
 #
-#     # 그외 일반적인 컬럼 정의
-#     street_name = Column(String(250))
-#     street_number = Column(String(250))
-#     post_code = Column(String(250), nullable+False)
+# # 세션생성, 2에서 생성한 engine
+# from sqlalchemy.orm import sessionmaker
+# # engine을 아직 생성하지 않은 경우 Session = sessionmaker()
+# Session = sessionmaker(bind=engine)
 #
-#     # 외래키를 지정한다
-#     person_id = Column(Integer, ForeignKey('person.id'))
+# # 두부와 연결을 해야할 때 인스턴트화를 한다.
+# session = Session()
 #
-#     # 외래키로 참조하는 테이블과의 관계를 기술
-#     person = relationship(Person)
-#
-#     # engine 생성<-- 데이터를 저장함
-#     engine = create_engine('oracle://localhost:1521/xe')
+# # Insert 또는 Update
+# session.add(ed_user)
